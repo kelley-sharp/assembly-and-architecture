2d1574082e00fef08670ea9dae62b5fcaee2a274
Kelley Sharp <kelleylsharp@gmail.com>
2021-02-08 22:51:31 -0800

    Project 3 done, but the calculations

--- Project3/Proj3_SharpKel.asm
+++ Project3/Proj3_SharpKel.asm
@@ -11,15 +11,18 @@ INCLUDE Irvine32.inc
 
 .data
 intro		BYTE "Hi, I'm Kelley. Welcome to the Integer Accumulation Game!", 0
-intro_2		BYTE "What is your name?", 0
+intro_2		BYTE "What is your name? ", 0
+rules_1     BYTE "Please enter a number in ranges [-200, -100] or [-50, -1].", 0
+rules_2		BYTE "Enter a non-negative number when you are finished to see results", 0
 username	BYTE 33 DUP(0) ;string from user input
 hello       BYTE "Hello ", 0
-instruct	BYTE ". Please enter a number in ranges [-200, -100] or [-50, -1].", 0
-notify		BYTE "That is and invalid negative number.", 0
+instruct	BYTE "Enter number: ", 0
+notify		BYTE "Number Invalid!", 0
 count		DWORD ?
 average		DWORD ?
 max			DWORD ?
 min 		DWORD ?
+sum			DWORD ?
 
 
 .code
@@ -37,46 +40,65 @@ main PROC
 	mov ECX, 32 ;specify max characters at 21
 	call ReadString
 
-
-; Greet user by name and display instructions
+; Greet user by name
 	mov EDX, OFFSET hello
 	call WriteString
 	mov EDX, OFFSET username
 	call WriteString
+	call CrLf
+	call CrLf
+
+; Display rules to the game
+	mov EDX, OFFSET rules_1
+	call CrLf
+	call WriteString
+	mov EDX, OFFSET rules_2
+	call CrLf
+	call WriteString
+	call CrLf
 
 ; Get current number from user
 _DisplayInstructions:
-	call WriteString
 	mov EDX, OFFSET instruct
 	call WriteString
 	call ReadInt
 
-	call ReadInt
-
-
 ; Validate the user input
-ifValid:
 	cmp EAX, -100
-	JG	_SecondCheck ; if the number is greater than -100
-	cmp EAX, -200
-	JL  _NotifyUser ; if the number is less than -200
-_SecondCheck:
-	cmp EAX, -50
-	JL _NotifyUser ; if the number is also less than -50
+	JE  _TallyNumber ; if the number is equal to -100
+	JG	_GreaterThan100Signed ; if the number is greater than -100
+	JL  _LessThan100Signed ; if the number is less than -100
+
+_GreaterThan100Signed:
 	cmp EAX, 0 
-	JAE _DataDisplay ; if the number is greater than or equal to zero
-	
-	; jump to DisplayInstructions
+	JGE _DisplayData ; if the number is greater than or equal to zero
+	cmp EAX, -50
+	JGE  _TallyNumber ; if the number is between -50 and -1
+	JL  _NotifyUser ; if the number is also less than -50 (between -100 and -50)
+
+_LessThan100Signed:
+	cmp EAX, -200
+	JGE  _TallyNumber ; if the number is in between -200 and -100
+	JL  _NotifyUser ; if the number is less than -200
+
+_TallyNumber:
+	; Increment Count
+	; Compute min/max
+	; Sum
+	; Compute current Rounded Avg
+	JMP _DisplayInstructions
 
 
 ; Notify the user of invalid negative numbers
 _NotifyUser:
 	mov EDX, OFFSET notify
 	call WriteString
+	call CrLf
+	JMP _DisplayInstructions
 
 
 ; Count and accumulate the valid user inputs
-_DataDisplay:
+_DisplayData:
 ; Calculate the (rounded integer) average of the valid numbers
 ; Display count, sum, max, min, and average of the valid numbers 
 ; Say goodbye (with the user's name)
