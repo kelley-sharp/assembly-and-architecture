b161ba0b94226fb819260da037c6cea57db72b08
Kelley Sharp <kelleylsharp@gmail.com>
2021-02-09 23:03:26 -0800

    finished project 3

--- Project3/Proj3_SharpKel.asm
+++ Project3/Proj3_SharpKel.asm
@@ -10,27 +10,47 @@ TITLE Data Validation, Looping, and Constants     (Proj3_SharpKel.asm)
 INCLUDE Irvine32.inc
 
 .data
+; Constants with text-equivalents for easier string interpolation
+LOWER_MIN   EQU -200
+LOWER_MIN_T EQU <"-200"> 
+LOWER_MAX   EQU -100
+LOWER_MAX_T EQU <"-100">
+UPPER_MIN   EQU -50
+UPPER_MIN_T EQU <"-50">
+UPPER_MAX   EQU -1
+UPPER_MAX_T EQU <"-1">
+
+; Intro & Prompt Strings
 intro		BYTE "Hi, I'm Kelley. Welcome to the Integer Accumulation Game!", 0
+xtra_cred	BYTE "**EC: Number the lines during user input. Increment the line number only for valid number entries", 0
 intro_2		BYTE "What is your name? ", 0
-rules_1     BYTE "Please enter a number in ranges [-200, -100] or [-50, -1].", 0
+rules_1     BYTE "Please enter a number in ranges [", LOWER_MIN_T, ",", LOWER_MAX_T, "] or [", UPPER_MIN_T, ",", UPPER_MAX_T, "].", 0
 rules_2		BYTE "Enter a non-negative number when you are finished to see results.", 0
 username	BYTE 33 DUP(0) ;string from user input
 hello       BYTE "Hello ", 0
+period		BYTE ". ", 0
 instruct	BYTE "Enter number: ", 0
 notify		BYTE "Number Invalid!", 0
+
+; These variables are for numeric calculations
 count		DWORD 0
 average		DWORD ?
 max			DWORD -201 ; set max to below the lower bound
 min 		DWORD 0 ; set min to above the upper bound
 sum			DWORD ?
+remainder	DWORD 0
+half        DWORD 2 ; for dividing by two
+
+; Summary & Conclusion Strings
 val_nums1	BYTE "You entered ", 0
 val_nums2	BYTE " valid numbers.", 0
 max_msg		BYTE "The maximum valid number is ", 0
 min_msg		BYTE "The minimum valid number is ", 0
 sum_msg		BYTE "The sum of your valid numbers is ", 0
 avg_msg		BYTE "The rounded average is ", 0
-remainder	DWORD 0
-half        DWORD 2
+no_nums		BYTE "No valid numbers entered.", 0 
+goodbye		BYTE "Farewell my dear ", 0
+exclaim		BYTE "!", 0
 
 
 .code
@@ -40,6 +60,10 @@ main PROC
 	mov EDX, OFFSET intro
 	call WriteString
 	call CrLf
+	mov EDX, OFFSET xtra_cred
+	call WriteString
+	call CrLf
+	
 
 ; Get the user's name
 	mov EDX, OFFSET intro_2
@@ -64,6 +88,11 @@ main PROC
 	call CrLf
 	call WriteString
 	call CrLf
+	;Do the first numbered line of user input
+	mov EAX, count
+	call writeDec
+	mov EDX, OFFSET period
+	call WriteString
 
 ; Get current number from user
 _DisplayInstructions:
@@ -72,22 +101,22 @@ _DisplayInstructions:
 	call ReadInt
 
 ; Validate the user input
-	cmp EAX, -100
-	JE  _TallyNumber ; if the number is equal to -100
-	JG	_GreaterThan100Signed ; if the number is greater than -100
-	JL  _LessThan100Signed ; if the number is less than -100
+	cmp EAX, LOWER_MAX
+	JE  _TallyNumber ; if the number is equal to LOWER_MAX
+	JG	_GreaterThanLowerMaxSigned ; if the number is greater than LOWER_MAX
+	JL  _LessThanLowerMaxSigned ; if the number is less than LOWER_MAX
 
-_GreaterThan100Signed:
+_GreaterThanLowerMaxSigned:
 	cmp EAX, 0 
 	JGE _DisplayData ; if the number is greater than or equal to zero
-	cmp EAX, -50
-	JGE  _TallyNumber ; if the number is between -50 and -1
-	JL  _NotifyUser ; if the number is also less than -50 (between -100 and -50)
+	cmp EAX, UPPER_MIN
+	JGE  _TallyNumber ; if the number is between UPPER_MIN and UPPER_MAX
+	JL  _NotifyUser ; if the number is also less than UPPER_MIN (between LOWER_MAX and UPPER_MIN)
 
-_LessThan100Signed:
-	cmp EAX, -200
-	JGE  _TallyNumber ; if the number is in between -200 and -100
-	JL  _NotifyUser ; if the number is less than -200
+_LessThanLowerMaxSigned:
+	cmp EAX, LOWER_MIN
+	JGE  _TallyNumber ; if the number is in between LOWER_MIN and LOWER_MAX
+	JL  _NotifyUser ; if the number is less than LOWER_MIN
 
 ; Do Computations
 _TallyNumber:
@@ -123,12 +152,25 @@ _CalculateAverage:
 	NEG remainder
 	cmp remainder, EAX
 	JGE _RoundUp
+	; Number the next line of user input
+	mov EAX, count
+	call writeDec
+	mov EDX, OFFSET period
+	call WriteString
+	; Prompt user again
 	JMP _DisplayInstructions
 
 ; Rounding "up" is actually decrementing b/c negative
 _RoundUp:
 	DEC average
+	; Number the next line of user input
+	mov EAX, count
+	call writeDec
+	mov EDX, OFFSET period
+	call WriteString
+	; Prompt user again
 	JMP _DisplayInstructions
+
 
 _UpdateMin:
 	mov min, EAX
@@ -144,11 +186,20 @@ _NotifyUser:
 	mov EDX, OFFSET notify
 	call WriteString
 	call CrLf
+	; Number the next line of user input
+	mov EAX, count
+	call writeDec
+	mov EDX, OFFSET period
+	call WriteString
+	; Prompt user again
 	JMP _DisplayInstructions
 
 ; Display data 
 _DisplayData:
-	; display the number of valid numbers entered
+	; Handle the case where no valid numbers are entered
+	cmp count, 0
+	JE _NoNumbers
+	; Display the number of valid numbers entered
 	mov EDX, OFFSET val_nums1
 	call WriteString
 	mov EAX, count
@@ -180,9 +231,33 @@ _DisplayData:
 	mov EAX, average
 	call WriteInt
 	call CrLf
+	JMP _SayGoodbye
+
+_NoNumbers:
+	mov EDX, OFFSET no_nums
+	call WriteString
+	call CrLf
+	;Do the first numbered line of user input again
+	mov EAX, count
+	call writeDec
+	mov EDX, OFFSET period
+	call WriteString
+	;Prompt user again
+	JMP _DisplayInstructions 
+
 
 
 ; Say goodbye (with the user's name)
+_SayGoodbye:
+	call CrLf
+	call CrLf
+	mov EDX, OFFSET goodbye
+	call WriteString
+	mov EDX, OFFSET username
+	call WriteString
+	mov EDX, OFFSET exclaim
+	call WriteString
+	call CrLf
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
